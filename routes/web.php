c7c5fd9a684e976893f6bc6292d1864bf8bffefe
Tosmak <tmak247@gmail.com>
2019-09-29 17:47:40 +0100

    Add route for transaction

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('transact');
 });
+
+Route::post('transact', 'TransactionController@initiateTransaction');
+Route::get('transact', 'TransactionController@displayPage');
