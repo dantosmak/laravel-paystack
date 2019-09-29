fc775b26c2cf111bc273dc42d18332bf36b60ea2
Tosmak <tmak247@gmail.com>
2019-09-29 17:45:44 +0100

    Create model for saving transaction to db

---
+++ app/Transaction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use SoftDeletes;
+    protected $fillable = ['fullname', 'email', 'amount'];
+}
